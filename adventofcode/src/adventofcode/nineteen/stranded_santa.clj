b90567754c4774e9628adf128eab5b202f70b2c6
Joel <hofgren.joel@gmail.com>
2019-12-02 23:07:55 +0100

    Day 2, part 1: Solved!

--- adventofcode/src/adventofcode/nineteen/stranded_santa.clj
+++ adventofcode/src/adventofcode/nineteen/stranded_santa.clj
@@ -29,19 +29,35 @@
 
 (defn opcode-sum
   [[i j k] data]
+  (println "opcode-sum " i j k)
   (assoc data k (+ (nth data i) (nth data j))))
 
 (defn opcode-multiply
   [[i j k] data]
+  (println "opcode-multiply " i j k)
   (assoc data k (* (nth data i) (nth data j))))
 
 (defn int-code-step
-  ([input] (int-code-step (take 4 input) input))
-  ([instruction data] (case (first instruction)
-                        99 data
-                        1 (opcode-sum (rest instruction) data)
-                        2 (opcode-multiply (rest instruction) data))))
+  ([data] (int-code-step 0 data))
+  ([instruction data]
+   (println "next instruction " instruction)
+   (case (nth data instruction)
+     99 data
+     1 (->> data
+            (opcode-sum (subvec data (inc instruction) (+ instruction 4)) ,,,)
+            (int-code-step (+ instruction 4) ,,,))
+     2 (->> data
+           (opcode-multiply (subvec data (inc instruction) (+ instruction 4)) ,,,)
+           (int-code-step (+ instruction 4) ,,,))))) 
 
-(defn int-code-computer
-  [input] (take 4 input))
+(defn prepare-input
+  []
+  (-> (mapv #(Integer. %) (-> "resources/nineteen/dayTwo.txt"
+                              (slurp)
+                              (string/trim)
+                              (string/split ,,, #",")))
+      (assoc ,,, 1 12)
+      (assoc ,,, 2 2)))
 
+(defn day-two-part-one
+  [] (first (int-code-step (prepare-input))))
