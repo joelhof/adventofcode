88e325be680bed33c2fdf126fe0849df3611e677
Joel <hofgren.joel@gmail.com>
2019-12-23 17:29:08 +0100

    Day 5, part 2: Support less than and equals instructions.

--- adventofcode/src/adventofcode/nineteen/int_computer.clj
+++ adventofcode/src/adventofcode/nineteen/int_computer.clj
@@ -8,9 +8,18 @@
 (defmethod parameter-value 1 [m i & program] i)
 
 (defn parameter-modes
-  [pointer program]
-  (reverse (drop-last 2 (digits (nth program pointer))))
+  [pointer program n]
+  (let [param-modes (drop-last 2 (digits (nth program pointer)))]
+    (reverse (concat 
+              (replicate (- n (count param-modes)) 0)
+              param-modes))
+    )
   )
+
+(defn get-parameter-values
+  [modes params program]
+  (map #(parameter-value (first %) (second %) program)
+       (partition 2 (interleave modes params))))
 
 ;(def pointer (atom 0))
 
@@ -31,7 +40,7 @@
   ;(println "+ pointer at:" pointer)
   (let [[i j k] (subvec program (inc pointer) (+ pointer 4))
         new-pointer (+ pointer 4)
-        modes (parameter-modes pointer program)]
+        modes (parameter-modes pointer program 4)]
     [new-pointer (assoc program k (+ (parameter-value (nth modes 0 0) i program)
                                      (parameter-value (nth modes 1 0) j program)))])
   )
@@ -40,7 +49,7 @@
   ;(println "* pointer at:" pointer)
   (let [[i j k] (subvec program (inc pointer) (+ pointer 4))
         new-pointer (+ pointer 4)
-        modes (parameter-modes pointer program)]
+        modes (parameter-modes pointer program 4)]
     [new-pointer (assoc program k (* (parameter-value (nth modes 0 0) i program)
                                      (parameter-value (nth modes 1 0) j program)))]
     )
@@ -71,7 +80,7 @@
 (defmethod pointer-instr 5 [pointer program]
   ; GOTO if non-zero
   (let [[i j] (subvec program (inc pointer) (+ 3 pointer))
-        modes (parameter-modes pointer program)
+        modes (parameter-modes pointer program 3)
         firstParam (parameter-value (nth modes 0 0) i program)
         new-pointer (if (not (zero? firstParam))
                       (parameter-value (nth modes 1 0) j program) (+ 3 pointer)) ]
@@ -81,11 +90,32 @@
 (defmethod pointer-instr 6 [pointer program]
   ; GOTO if non-zero
   (let [[i j] (subvec program (inc pointer) (+ 3 pointer))
-        modes (parameter-modes pointer program)
+        modes (parameter-modes pointer program 3)
         firstParam (parameter-value (nth modes 0 0) i program)
         new-pointer (if (zero? firstParam)
                       (parameter-value (nth modes 1 0) j program) (+ 3 pointer))]
     [new-pointer program]))
+
+(defn predicate
+  [f pointer program]
+  ; f takes 2 args returning a boolean.
+  ; If f returns true, store 1 at instructions 3:rd parameter. 
+  (let [params (subvec program (inc pointer) (+ 4 pointer))
+        modes (parameter-modes pointer program 3)
+        values (get-parameter-values modes params program)]
+    [(+ 4 pointer) (assoc program (last params)
+                          (if (f (first values) (second values))
+                            1
+                            0))])
+  )
+
+(defmethod pointer-instr 7 [pointer program]
+  ; LESS THAN
+  (predicate < pointer program))
+
+(defmethod pointer-instr 8 [pointer program]
+  ; EQUALS
+  (predicate = pointer program))
 
 (defn run
   ([program] (run 0 program))
