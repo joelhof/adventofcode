15da8d876a836eca7dc0a6a5363ad6737952aaf5
Joel <hofgren.joel@gmail.com>
2019-12-28 16:40:04 +0100

    Day 7, part 2: Refactor to use channels for communication instead of atoms.
    Channels are passed as arguments, so caller is responsible for managing the channels.
    Except out-chan, which is closed when computation halts.
    Note use of map desctructuring in function arguments.

--- adventofcode/src/adventofcode/nineteen/int_computer.clj
+++ adventofcode/src/adventofcode/nineteen/int_computer.clj
@@ -1,4 +1,5 @@
-(ns adventofcode.nineteen.int-computer)
+(ns adventofcode.nineteen.int-computer
+  (:require [clojure.core.async :as async]))
 
 (defn digits [number]
   (rseq (mapv #(mod % 10) (take-while pos? (iterate #(quot % 10) number)))))
@@ -26,115 +27,140 @@
 (def input (atom 0))
 (def output (atom 0))
 
+(def input-chan (async/chan 2))
+(def output-chan (async/chan 1))
+
 (defn get-opcode
   [instr]
   (mod instr 100)
   )
 
-(defmulti pointer-instr (fn [pointer program] (get-opcode
-                                               (nth program pointer))))
-(defmethod pointer-instr 1 [pointer program]
-  ;(println "+ pointer at:" pointer)
-  (let [[i j k] (subvec program (inc pointer) (+ pointer 4))
-        new-pointer (+ pointer 4)
-        modes (parameter-modes pointer program 3)]
-    [new-pointer (assoc program k (+ (parameter-value (nth modes 0 0) i program)
-                                     (parameter-value (nth modes 1 0) j program)))])
-  )
+(defn update-state [m p i value]
+  (-> m
+      (assoc-in ,,, [:pointer] p)
+      (assoc-in ,,, [:program i] value)))
 
-(defmethod pointer-instr 2 [pointer program]
-  ;(println "* pointer at:" pointer)
+(defmulti pointer-instr (fn [{:keys [pointer program]}] (get-opcode
+                                               (nth program pointer))))
+(defmethod pointer-instr 1 [{:keys [pointer program] :as m}]
+  (println "+ pointer at:" pointer)
   (let [[i j k] (subvec program (inc pointer) (+ pointer 4))
         new-pointer (+ pointer 4)
         modes (parameter-modes pointer program 3)]
-    [new-pointer (assoc program k (* (parameter-value (nth modes 0 0) i program)
-                                     (parameter-value (nth modes 1 0) j program)))]
+    (update-state m new-pointer k (+ (parameter-value (nth modes 0 0) i program)
+                                     (parameter-value (nth modes 1 0) j program)))
     )
   )
 
-(defmethod pointer-instr 99 [pointer program]
-  ; XXX think about how to signal program halt.
-  ;(println "Halting! pointer at:" pointer)
-  [pointer program])
-
-(defmethod pointer-instr 3 [pointer program]
-  ; Store input in memory
-  ;(println "Storing " @input "@ pos " pointer)
-  (let [i (nth program (inc pointer))
-        input-value (peek @input)]
-    (swap! input pop)
-    [(+ pointer 2) (assoc program i input-value)])
+(defmethod pointer-instr 2 [{:keys [pointer program] :as m}]
+  (println "* pointer at:" pointer)
+  (let [[i j k] (subvec program (inc pointer) (+ pointer 4))
+        new-pointer (+ pointer 4)
+        modes (parameter-modes pointer program 3)]
+    (update-state m new-pointer k (* (parameter-value (nth modes 0 0) i program)
+                                     (parameter-value (nth modes 1 0) j program)))
+    )
   )
 
-(defmethod pointer-instr 4 [pointer program]
+(defmethod pointer-instr 99 [{:keys [pointer out-chan] :as m}]
+  ; XXX think about how to signal program halt.
+  (println "Halting! pointer at:" pointer)
+  (async/close! out-chan)
+  (assoc m :exit true)
+  )
+
+(defmethod pointer-instr 3 [{:keys [pointer program in-chan] :as m}]
+  ; Store input in memory
+  (println "Reading input..." pointer)
+  (let [i (nth program (inc pointer))
+        input-value (async/<!! in-chan)]
+    (println "Input" input-value "recieved. Proceeding...")
+    (update-state m (+ pointer 2) i input-value)
+    ))
+
+(defmethod pointer-instr 4 [{:keys [pointer program out-chan] :as m}]
   ; Output value from memory
-  ;(println "output instruction...")
+  (println "output instruction..." pointer)
   (let [new-pointer (inc pointer)
         modes (parameter-modes pointer program 1)
         i (nth program new-pointer)]
-    (reset! output (parameter-value (nth modes 0 0) i program))
-    [(inc new-pointer) program]
-    )
-  )
+    (async/put! out-chan (parameter-value (nth modes 0 0) i program))
+    (assoc m :pointer (+ pointer 2))
+    ))
 
-(defmethod pointer-instr 5 [pointer program]
+(defmethod pointer-instr 5 [{:keys [pointer program] :as m}]
   ; GOTO if non-zero
   (let [[i j] (subvec program (inc pointer) (+ 3 pointer))
         modes (parameter-modes pointer program 2)
         firstParam (parameter-value (nth modes 0 0) i program)
         new-pointer (if (not (zero? firstParam))
-                      (parameter-value (nth modes 1 0) j program) (+ 3 pointer)) ]
-    [new-pointer program])
+                      (parameter-value (nth modes 1 0) j program) (+ 3 pointer))]
+    (assoc m :pointer new-pointer)
+    )
   )
 
-(defmethod pointer-instr 6 [pointer program]
+(defmethod pointer-instr 6 [{:keys [pointer program] :as m}]
   ; GOTO if non-zero
   (let [[i j] (subvec program (inc pointer) (+ 3 pointer))
         modes (parameter-modes pointer program 2)
         firstParam (parameter-value (nth modes 0 0) i program)
         new-pointer (if (zero? firstParam)
                       (parameter-value (nth modes 1 0) j program) (+ 3 pointer))]
-    [new-pointer program]))
+    (assoc m :pointer new-pointer)))
 
 (defn predicate
-  [f pointer program]
+  [f {:keys [pointer program] :as m}]
   ; f takes 2 args returning a boolean.
   ; If f returns true, store 1 at instructions 3:rd parameter. 
   (let [params (subvec program (inc pointer) (+ 4 pointer))
         modes (parameter-modes pointer program 3)
         values (get-parameter-values modes params program)]
-    [(+ 4 pointer) (assoc program (last params)
-                          (if (f (first values) (second values))
-                            1
-                            0))])
-  )
+    (update-state m (+ 4 pointer) (last params) (if (f
+                                                     (first values) (second values))
+                                                  1
+                                                  0)))
+   ;  [(+ 4 pointer) (assoc program (last params)
+   ;                        (if (f (first values) (second values))
+   ;                          1
+   ;                          0))])
+    )
 
-(defmethod pointer-instr 7 [pointer program]
+(defmethod pointer-instr 7 [state]
   ; LESS THAN
   ;(println "LESS THAN instruction")
-  (predicate < pointer program))
+  (predicate < state))
 
-(defmethod pointer-instr 8 [pointer program]
+(defmethod pointer-instr 8 [state]
   ; EQUALS
   ;(println "EQUALS instruction")
-  (predicate = pointer program))
+  (predicate = state))
 
 (defn run
-  ([program] (run 0 program))
+  ([program] (run 0 {:pointer 0 :program program} ))
+  ([program input< output>] (run 0 {
+                                    :pointer 0
+                                    :program program 
+                                    :in-chan input< 
+                                    :out-chan output>}))
   ([pointer program]
    ;(println pointer program)
-   (let [[new-pointer new-prog] (pointer-instr pointer program)]
-     (if (= pointer new-pointer)
+   (let [new-prog (pointer-instr program)]
+     (if (:exit new-prog)
        new-prog
-       (recur new-pointer new-prog))
+       (recur 0 new-prog))
      )
    ))
 
 (defn amplify!
   [phase input-value program]
-  (reset! input (list phase input-value))
-  (run program)
-  @output
+  (let [in-chan (async/chan 2)
+        out-chan (async/chan 1)]
+    (async/>!! in-chan phase)
+    (async/>!! in-chan input-value)
+    (run program in-chan out-chan)
+    (async/<!! out-chan))
+  ;(reset! input (list phase input-value))
+  
   )
 
 (def test-program [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99])
