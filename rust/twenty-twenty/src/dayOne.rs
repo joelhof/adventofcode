79cf703b11661c3e08a7ded24ee8dc9dd0783595
Joel <hofgren.joel@gmail.com>
2020-12-02 00:17:36 +0100

    Day 1, part 2: Solved!
    Sometimes imperative loops are just easier.

--- rust/twenty-twenty/src/dayOne.rs
+++ rust/twenty-twenty/src/dayOne.rs
@@ -1,12 +1,6 @@
 
 pub fn day_one(input: &str) -> i64 {
-    let parseResult: Result<Vec<i64>, _> = input.split("\n")
-            .map(|line| line.trim())
-            .filter(|line| !line.is_empty())
-            .map(|line| line.parse::<i64>())
-            .collect();
-    let numbers = parseResult.unwrap();
-    println!("{:?}", numbers);
+    let numbers = parseInput(input);
     let sumsToTwenty: i64 = numbers.clone()
         .into_iter()
         .filter(|n| sum_vector(n, &numbers)
@@ -14,6 +8,31 @@ pub fn day_one(input: &str) -> i64 {
             .any(|sum| *sum == 2020))
         .fold(1, |acc, x| acc * x);
     return sumsToTwenty;
+}
+
+pub fn dayOnePartTwo(input: &str) -> i64 {
+    let numbers = parseInput(input);
+    let mut product = 0;
+    for i in &numbers {
+        for j in &numbers {
+            for k in &numbers {
+                let sum = i + j + k;
+                if sum == 2020 {
+                    product = i * j * k;
+                }
+            }
+        }
+    }
+    return product;
+} 
+
+fn parseInput(input: &str) -> Vec<i64> {
+    let parseResult: Result<Vec<i64>, _> = input.split("\n")
+            .map(|line| line.trim())
+            .filter(|line| !line.is_empty())
+            .map(|line| line.parse::<i64>())
+            .collect();
+    return parseResult.unwrap();
 }
 
 fn sum_vector(n: &i64, numbers: &[i64]) -> Vec<i64> {
@@ -47,5 +66,18 @@ mod tests {
             1456";
         let result = day_one(INPUT);
         assert_eq!(result, 514579i64);
+    }
+
+    #[test]
+    fn findThreeNumbers() {
+        const INPUT: &str = "1721
+            979
+            366
+            299
+
+            675
+            1456";
+        let result = dayOnePartTwo(INPUT);
+        assert_eq!(result, 241861950);
     }
 }
--- rust/twenty-twenty/src/main.rs
+++ rust/twenty-twenty/src/main.rs
@@ -1,3 +1,4 @@
+#![allow(non_snake_case)]
 use twentytwenty::dayOne;
 use std::path::PathBuf;
 use std::fs;
@@ -6,7 +7,10 @@ fn main() {
     let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     d.push("resources");
     d.push("dayOne.txt");
-    let day_one_input = fs::read_to_string(d);
-    let result = dayOne::day_one(&day_one_input.unwrap()[..]);
+    let day_one_input = fs::read_to_string(d).unwrap();
+    let result = dayOne::day_one(&day_one_input[..]);
     println!("Day One, part 1: {:?}", result);
+
+    let result = dayOne::dayOnePartTwo(&day_one_input[..]);
+    println!("Day One, part 2: {:?}", result);
 }
