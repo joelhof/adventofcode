1be4fb87bfa7ee65e18c831f7f5b112ce5d77819
Joel <hofgren.joel@gmail.com>
2020-12-01 23:18:13 +0100

    Day 1: Test passes.
    Quite fiddly to get the double loop working with all the borrowing and what not.

--- rust/twenty-twenty/src/dayOne.rs
+++ rust/twenty-twenty/src/dayOne.rs
@@ -1,16 +1,30 @@
 
 pub fn day_one(input: &str) -> i64 {
-    return -1i64;
+    let parseResult: Result<Vec<i64>, _> = input.split("\n")
+            .map(|line| line.trim())
+            .map(|line| line.parse::<i64>())
+            .collect();
+    let numbers = parseResult.unwrap();
+    let sumsToTwenty: i64 = numbers.clone()
+        .into_iter()
+        .filter(|n| sum_vector(n, &numbers)
+            .iter()
+            .any(|sum| *sum == 2020))
+        .fold(1, |acc, x| acc * x);
+    return sumsToTwenty;
 }
 
+fn sum_vector(n: &i64, numbers: &[i64]) -> Vec<i64> {
+    return numbers.iter().map(|x| x + n).collect();
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn it_works() {
-        const INPUT: &str = "
-            1721
+        const INPUT: &str = "1721
             979
             366
             299
