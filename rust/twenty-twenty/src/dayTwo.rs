06c58899b5a0c6d1b5a29adf4059e545dec05f26
Joel <hofgren.joel@gmail.com>
2020-12-02 15:03:17 +0100

    Day 2, part 1: Parse policy.

--- rust/twenty-twenty/src/dayTwo.rs
+++ rust/twenty-twenty/src/dayTwo.rs
@@ -9,7 +9,29 @@ pub fn solve(input: &str) -> u32 {
 }
 
 fn validPassword(input: &str) -> u32 {
+    let tmp: Vec<&str> = input.split(":").collect();
+    let policy = Policy::new(tmp[0]);
+    let password = tmp[1];
+    println!("policy {:?} password {:2}", policy, password);
+
     return 0;
+}
+
+#[derive(Debug, Clone, PartialEq)]
+struct Policy {
+    min: u32,
+    max: u32,
+    key: String
+}
+
+impl Policy {
+    pub fn new(input: &str) -> Policy {
+        return Policy {
+            min: input[0..1].parse().unwrap(),
+            max: input[2..3].parse().unwrap(),
+            key: input[4..].to_string()
+        };
+    }
 }
 
 #[cfg(test)]
