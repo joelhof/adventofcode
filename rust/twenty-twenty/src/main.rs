8ca010fb64c7b9be5e5a762784c9508fdc5c4f8c
Joel <hofgren.joel@gmail.com>
2020-12-02 16:28:45 +0100

    Day 2, part 1: Solved!

--- rust/twenty-twenty/src/main.rs
+++ rust/twenty-twenty/src/main.rs
@@ -1,9 +1,15 @@
 #![allow(non_snake_case)]
 use twentytwenty::dayOne;
+use twentytwenty::dayTwo;
 use std::path::PathBuf;
 use std::fs;
 
 fn main() {
+    dayOne();
+    dayTwo();
+}
+
+fn dayOne() {
     let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     d.push("resources");
     d.push("dayOne.txt");
@@ -13,4 +19,13 @@ fn main() {
 
     let result = dayOne::dayOnePartTwo(&day_one_input[..]);
     println!("Day One, part 2: {:?}", result);
+}
+
+fn dayTwo() {
+    let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    d.push("resources");
+    d.push("dayTwo.txt");
+    let day_two_input = fs::read_to_string(d).unwrap();
+    let result = dayTwo::solve(&day_two_input[..]);
+    println!("Day Two, part 1: {:?}", result);
 }
