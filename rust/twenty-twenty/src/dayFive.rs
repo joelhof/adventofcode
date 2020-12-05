481f7bf5ce26fc7ef0caf057e2404d69098e4397
Joel <hofgren.joel@gmail.com>
2020-12-05 21:33:03 +0100

    Day 5, part 1: Solved examples.
    Neat recursion and pattern match. Constructing arrays was a bit cumbersome though.

--- rust/twenty-twenty/src/dayFive.rs
+++ rust/twenty-twenty/src/dayFive.rs
@@ -3,7 +3,6 @@
 trait AdventOfCodeProblem {
     fn partOne(&self) -> u32;
     fn partTwo(&self) -> u32;
-    
 }
 
 struct DayFive {
@@ -17,20 +16,39 @@ impl DayFive {
         }
     }
 
-    fn findSeat(&self, seat: &str) -> (u32, u32) {
-        return (1,1);
+    fn findSeat(&self, seat: &str, seatRange: &[u32], columnRange: &[u32]) -> (u32, u32) {
+        let half = if seatRange.len() > 1 { seatRange.len() / 2 } else { columnRange.len() / 2 };
+        if seat.is_empty() {
+            return (seatRange[0], columnRange[0]);
+        }
+        let pos = &seat[0..1];
+        return match pos {
+            "F" => self.findSeat(&seat[1..], &seatRange[0..half], columnRange),
+            "B" => self.findSeat(&seat[1..], &seatRange[half..], columnRange),
+            "L" => self.findSeat(&seat[1..], &seatRange, &columnRange[0..half]),
+            "R" => self.findSeat(&seat[1..], &seatRange, &columnRange[half..]),
+            _ => (0,0)
+        };
     }
 }
 
 impl AdventOfCodeProblem for DayFive {
 
     fn partOne(&self) -> u32 {
+        let mut seatRange =  [0; 128];
+        for i in 0..128 {
+            seatRange[i] = i as u32;
+        }
+        let mut columnRange =  [0; 8];
+        for i in 0..8 {
+            columnRange[i] = i as u32;
+        }
         return match self.input.split("\n")
             .map(|line| line.trim())
-            .map(|seat| self.findSeat(seat))
+            .map(|seat| self.findSeat(seat, &seatRange, &columnRange))
             .map(|seatNr| {
                 let (row, column) = seatNr;
-                return 44 * row + column;
+                return 8 * row + column;
             })
             .max() {
                 Some(x) => x as u32,
@@ -49,7 +67,8 @@ mod tests {
 
     #[test]
     fn partOneExampleTest() {
-        const INPUT: &str = "BFFFBBFRRR
+        const INPUT: &str = "FBFBBFFRLR
+        BFFFBBFRRR
         FFFBBBFRRR
         BBFFBBFRLL" ;
         let result = DayFive::new(INPUT).partOne();
