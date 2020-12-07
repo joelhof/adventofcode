be2e702fb769decf5854d624a105eb53d00860d0
Joel <hofgren.joel@gmail.com>
2020-12-07 23:03:29 +0100

    Day 7, part 1: Solve example.
    Solve by recursive DFS.

--- rust/twenty-twenty/src/daySeven.rs
+++ rust/twenty-twenty/src/daySeven.rs
@@ -3,9 +3,11 @@ extern crate regex;
 
 use crate::core::*;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use regex::Regex;
 pub struct DaySeven {
-    bagGraph: HashMap<String, Vec<String>>
+    bagGraph: HashMap<String, Vec<String>>,
+
 }
 
 impl DaySeven {
@@ -14,10 +16,55 @@ impl DaySeven {
             bagGraph: constructGraph(input)
         }
     }
+
+    fn depthFirstSearch(&self, target: &str) -> HashSet<String> {
+        let mut reachesTarget: HashSet<String> = HashSet::new();
+        for (node, _) in &self.bagGraph {
+            println!("node {}", node);
+            if target == node {
+                continue;
+            }
+            let mut visited: HashSet<String> = HashSet::new();
+            self.dfs(node, target, &mut visited);
+            if visited.iter().any(|child| child == target) {
+                reachesTarget.insert(String::from(node));
+            }
+        }
+        println!("{:?}", reachesTarget);
+        return reachesTarget;
+    }
+
+    fn dfs(&self, source: &str, target: &str, visited: &mut HashSet<String>) {
+        println!("node {}", source);
+        let children = self.bagGraph.get(source).unwrap();
+        //if children.iter().any(|child| child == target) {
+        //println!("found {} from source {}", target, source);
+        visited.insert(source.to_string());
+        //}
+        children.iter()
+            .for_each(|c| self.dfs(c, target, visited));
+    }
+}
+
+impl AdventOfCodeSolver for DaySeven {
+    fn day(&self) -> &str {
+        return "Seven";
+    }
+
+    fn partOne(&self) -> u32 {
+        let target = "shiny gold";
+        return self.depthFirstSearch(target).len() as u32;
+    }
+
+    fn partTwo(&self) -> u32 {
+        return 0;
+    }
 }
 
 fn constructGraph(input: &str) -> HashMap<String, Vec<String>> {
-    let graph: HashMap<String, Vec<String>> = input.split("\n")
+    let bag = Regex::new(r" bag[s]?").unwrap();
+    let graph: HashMap<String, Vec<String>> = bag.replace_all(input, "")
+        .split("\n")
         .map(|line| {
             let n: Vec<_> = line.split("contain").collect();
             return (match n.get(0) {
@@ -35,30 +82,16 @@ fn getChildren(childOpt: Option<&&str>) -> Vec<String> {
         Some(s) => s.to_string(),
         None => String::from("")
     };
-    let re = Regex::new(r"[0-9]").unwrap();
+    let numbers = Regex::new(r"[0-9]").unwrap();
     let children: Vec<String> = childString.split(",")
-        .map(|child| re.replace_all(child, "")
+        .map(|child| numbers.replace_all(child, "")
                 .replace(".", "")
-                .replace("no other bags", "")
+                .replace("no other", "")
                 .trim()
                 .to_string())
         .filter(|child| !child.is_empty())
         .collect();
     return children;
-}
-
-impl AdventOfCodeSolver for DaySeven {
-    fn day(&self) -> &str {
-        return "Seven";
-    }
-
-    fn partOne(&self) -> u32 {
-        return 0;
-    }
-
-    fn partTwo(&self) -> u32 {
-        return 0;
-    }
 }
 
 #[cfg(test)]
