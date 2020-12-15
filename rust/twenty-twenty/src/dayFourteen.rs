3ef293a38750e95c900a13e4814f05df28546b51
Joel <hofgren.joel@gmail.com>
2020-12-15 18:24:44 +0100

    Day 14, part 2: Solved!
    Found a much better bitmasking solution compared to previous years when I iterated over characters in a string.

--- rust/twenty-twenty/src/dayFourteen.rs
+++ rust/twenty-twenty/src/dayFourteen.rs
@@ -2,6 +2,7 @@
 
 use crate::core::*;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub struct PartOne {
    input: String
@@ -38,6 +39,25 @@ fn parseMemoryInstruction(line: &str) -> (u64, u64) {
 fn applyMask(value: &u64, zerosMask: &u64, onesMask: &u64) -> u64 {
     return zerosMask & (onesMask | value);
 }
+
+struct Version2Decoder {
+    onesMask: u64,
+    wildcardMask: String
+}
+
+impl Version2Decoder {
+    fn fromString(input: &str) -> Version2Decoder {
+        let mask = if input.len() > 0 { parseMask(input, "0") } else { 0 };
+        let wildcard = match input.split("=").nth(1) {
+            Some(m) => m.trim().to_string(),
+            None => "".to_string()
+        };
+        return Version2Decoder {
+            wildcardMask: wildcard,
+            onesMask: mask,
+        }
+    }
+}
  
 impl AdventOfCodeSolver for PartOne {
     fn day(&self) -> &str {
@@ -55,6 +75,37 @@ impl AdventOfCodeSolver for PartOne {
             } else if line.trim().starts_with("mem") {
                 let instruction = parseMemoryInstruction(line);
                 memoryArray.insert(instruction.0, applyMask(&instruction.1, &zerosMask, &onesMask));
+            }
+        }
+        return memoryArray.iter().map(|(_k, v)| v).sum();
+    }
+
+    fn partTwo(&self) -> u64 {
+        let mut decoder: Version2Decoder = Version2Decoder::fromString("");
+        let mut memoryArray: HashMap<u64, u64> = HashMap::new();
+        let base: u64 = 2;
+        for line in self.input.split("\n") {
+            //println!("line {}", line);
+            if line.trim().starts_with("mask") {
+                decoder = Version2Decoder::fromString(line);
+            } else if line.trim().starts_with("mem") {
+                let instruction = parseMemoryInstruction(line);
+                let mappedValue = decoder.onesMask | instruction.0;
+                let mut possibleValues: HashSet<u64> = HashSet::new();
+                possibleValues.insert(mappedValue);
+                decoder.wildcardMask.chars().rev()
+                    .enumerate()
+                    .for_each(|(i, mask)| {
+                        if mask == 'X' {
+                            let newValues: HashSet<u64> = possibleValues.iter()
+                                .map(|v| base.pow(i as u32) ^ v)
+                                .collect();
+                            possibleValues.extend(newValues);
+                        }
+                    });
+                for addr in possibleValues.into_iter() {
+                    memoryArray.insert(addr, instruction.1);
+                }
             }
         }
         return memoryArray.iter().map(|(_k, v)| v).sum();
@@ -88,5 +139,15 @@ mod tests {
         mem[57760] = 2161095";
         let result = PartOne::init(INPUT).partOne();
         assert_eq!(result, 377131941922);
+    }
+
+    #[test]
+    fn fourTeenPartTwoExampleTest() {
+        const INPUT: &str = "mask = 000000000000000000000000000000X1001X
+        mem[42] = 100
+        mask = 00000000000000000000000000000000X0XX
+        mem[26] = 1";
+        let result = PartOne::init(INPUT).partTwo();
+        assert_eq!(result, 208);
     }
 }
