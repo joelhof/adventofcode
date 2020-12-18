572ed357c5e11f770ba95f4e9f6068eaa7f703c3
Joel <hofgren.joel@gmail.com>
2020-12-18 22:23:31 +0100

    Day 18, part 1: Simple, unnested test solved.

--- rust/twenty-twenty/src/dayEighteen.rs
+++ rust/twenty-twenty/src/dayEighteen.rs
@@ -2,6 +2,7 @@
 
 use crate::core::*;
 use std::str::FromStr;
+use regex::Regex;
 
 struct Day {
     input: Vec<String>
@@ -12,10 +13,10 @@ impl FromStr for Day {
 
     fn from_str(input: &str) -> Result<Self, Self::Err> {
         return Ok(Day {
-            input:  input.split("\n")
+            input: input.split("\n")
                         .map(|line| line.trim())
                         .filter(|line| !line.is_empty())
-                        .map(|line| line.to_string())
+                        .map(|line| line.replace(" ", "").to_string())
                         .collect()
         });
     }
@@ -25,6 +26,60 @@ impl AdventOfCodeSolver for Day {
     fn day(&self) -> &str {
         return "Eighteen";
     }
+
+    fn partOne(&self) -> u64 {
+        let mut results: u64 = self.input.iter()
+            .map(|line| line.chars().collect::<Vec<char>>())
+            .map(|line| eval(&mut line.to_vec()))
+            .sum();
+        println!("{:?}", results);
+        // let evals: Vec<u64> = expressions.iter()
+        //     .map(|expr| eval(&mut expr.to_vec()))
+        //     .collect();
+        return results;
+    }
+}
+
+struct Expr {
+    lhs: u64,
+    operator: String,
+    rhs: u64
+}
+
+fn eval(expr: &mut Vec<char>) -> u64 {
+    //let mut lhs: u64;
+    let mut rhs: u64;
+    let mut res: u64 = 0;
+    let lhs = expr.pop();
+    println!("{:?}", lhs);
+    if lhs.is_some() {
+        if char::is_numeric(lhs.unwrap()) {
+            let lhsArg = lhs.unwrap().to_string().parse().unwrap();
+            match expr.pop() {
+                None => return lhsArg,
+                Some(op) if '+' == op => return lhsArg + eval(expr),
+                Some(op) if '*' == op => return lhsArg * eval(expr),
+                Some(_) => return eval(expr)
+            }
+        } else {
+            res = eval(expr);
+        }
+    } else {
+        println!("end of line reached");
+        return res;
+    }
+    return res;
+    // for c in expr.into_iter() {
+    //     match *c {
+    //         "(" => ,
+    //         "+" => lhs + eval(),
+    //         "*" => lhs * eval(),
+    //         _ => 0
+    // //         c if char::is_numeric => // then c is an argument
+    // //         "*" => , // then operator is '*'
+    // //         "+" => 
+    //     }
+    // }
 }
 
 #[cfg(test)]
@@ -34,6 +89,13 @@ mod tests {
     #[test]
     fn eighteenPartOneSimpleExampleTest() {
         const INPUT: &str = "1 + 2 * 3 + 4 * 5 + 6";
+        let result = INPUT.parse::<Day>().unwrap().partOne();
+        assert_eq!(result, 71);
+    }
+
+    #[test]
+    fn eighteenPartOneParTestExampleTest() {
+        const INPUT: &str = "1 + (2 * 3) + (4 * (5 + 6))";
         let result = INPUT.parse::<Day>().unwrap().partOne();
         assert_eq!(result, 71);
     }
