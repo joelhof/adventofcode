382a68d3ca2623d86146ab14645cc71bd6cea6ce
Joel <hofgren.joel@gmail.com>
2021-01-06 23:26:15 +0100

    Day 4, part 2: Solved!
    Stumbled a bit on the pid reg ex. Should've written a unit test for that field...
    Pretty happy with solution. Nice to get a Result with a list of all the errors.
    Would've been even nice to get a Result<Passport, Vec<Error>>, but that required more
    Rustiness than I can muster at the moment.

--- rust/twenty-twenty/src/dayFour.rs
+++ rust/twenty-twenty/src/dayFour.rs
@@ -4,7 +4,11 @@ use crate::core::*;
 use std::str::FromStr;
 use regex::Regex;
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::fmt;
 use std::error::Error;
+use lazy_static::lazy_static;
+
 
 pub struct Day {
     input: Vec<String>
@@ -51,15 +55,22 @@ impl AdventOfCodeSolver for Day {
             .filter(|passport| !passport.is_empty())
             .map(|passport| passport.parse::<Passport>())
             .collect();
-        //println!("{:?}", passports);
         
         return passports.iter().filter(|passport| passport.is_ok()).count() as u64;
+    }
+
+    fn partTwo(&self) -> u64 {
+        return self.input.iter()
+            .filter(|passport| !passport.is_empty())
+            .map(|passport| passport.parse::<Passport>())
+            .filter_map(|passport| passport.ok())
+            .filter_map(|passport| passport.validate().ok())
+            .count() as u64;
     }
 }
 
 #[derive(Debug, PartialEq, Clone)]
 struct Passport {
-    
     birthYear: String,
     issueYear: String,
     expirationYear: String,
@@ -70,13 +81,16 @@ struct Passport {
     countryID: Option<u32>
 }
 
+lazy_static! {
+    static ref FIELD_VALUE_RE: Regex = Regex::new(r"(?P<field>\w+):(?P<value>\w+|\#\w+)").unwrap();
+}
+
 impl FromStr for Passport {
     type Err = Box<dyn Error>;
     
     fn from_str(input: &str) -> Result<Self, Self::Err> {
-        let rangePattern = Regex::new(r"(?P<field>\w+):(?P<value>\w+|\#\w+)").unwrap();
-        //let fields = vec!["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"];
-        let map: HashMap<String, String> = rangePattern.captures_iter(input)
+        
+        let map: HashMap<String, String> = FIELD_VALUE_RE.captures_iter(input)
             .map(|captures|{ 
                 let f = &captures["field"];
                 let v =  &captures["value"];
@@ -95,6 +109,98 @@ impl FromStr for Passport {
         });
     }
 }
+
+lazy_static! {
+    static ref HEIGTH_RE: Regex = Regex::new(r"(([0-9]{2,3})(in|cm)\b)").unwrap();
+    static ref HAIR_COLOR_RE: Regex = Regex::new(r"(#[0-9a-f]{6}\b)").unwrap();
+    static ref EYE_COLOR_RE: Regex = Regex::new(r"(amb\b|blu\b|brn\b|gry\b|grn\b|hzl\b|oth\b)").unwrap();
+    static ref PASSPORT_ID_RE: Regex = Regex::new(r"(^[0-9]{9}$)").unwrap();
+}
+type Err = Box<dyn Error>;
+
+#[derive(Debug, Clone)]
+struct InvalidFieldValue(String);
+
+impl fmt::Display for InvalidFieldValue {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl Error for InvalidFieldValue {}
+impl Passport {
+
+    pub fn validate(&self) -> Result<bool, Vec<Err>> {
+        let mut errors: Vec<Err> = Vec::new();
+
+        if !Self::isValidNumber(&self.birthYear, 1920, 2002) {
+            errors.push(Box::new(InvalidFieldValue(format!("Invalid 'BirthYear' {}", self.birthYear).to_string())));
+        }
+        if !Self::isValidNumber(&self.issueYear, 2010, 2020) {
+            errors.push(Box::new(InvalidFieldValue(format!("Invalid 'IssueYear' {}", self.issueYear).to_string())));
+        }
+        if !Self::isValidNumber(&self.expirationYear, 2020, 2030) {
+            errors.push(Box::new(InvalidFieldValue(format!("Invalid 'ExpirationYear' {}", self.expirationYear).to_string())))
+        }
+        if !self.isValidHeight() {
+            errors.push(Box::new(InvalidFieldValue(format!("Invalid 'Height' {}", self.height).to_string())))
+        }
+        if !self.isValidEyeColor() {
+            errors.push(Box::new(InvalidFieldValue(format!("Invalid 'EyeColor' {}", self.eyeColor).to_string())))
+        }
+        if !self.isValidHairColor() {
+            errors.push(Box::new(InvalidFieldValue(format!("Invalid 'HairColor' {}", self.hairColor).to_string())))
+        }
+        if !self.isValidId() {
+            errors.push(Box::new(InvalidFieldValue(format!("Invalid 'Id' {}", self.passportID).to_string())))
+        };
+        
+        return if errors.len() > 0 { Err(errors) } else { Ok(true) };
+    }
+
+    fn isValidNumber(nr: &str, min: u32, max: u32) -> bool {
+        return match nr.parse::<u32>() {
+            Ok(nr) if nr >= min && nr <= max => true,
+            _ => false
+        };
+    }
+
+    fn isValidHeight(&self) -> bool {
+        return match HEIGTH_RE.captures(&self.height) {
+            Some(cap) => {
+                let h = cap.get(2).map_or("", |m| m.as_str());
+                cap.get(3).map_or(false, |m| match m.as_str() { 
+                    "in" => Self::isValidNumber(h, 59, 76),
+                    "cm" => Self::isValidNumber(h, 150, 193),
+                    _ => false
+                 })
+            },
+            None => false  
+        };
+    }
+
+    fn isValidEyeColor(&self) -> bool {
+        return match EYE_COLOR_RE.captures(&self.eyeColor) {
+            Some(_cap) => true,
+            None => false
+         }
+    }
+
+    fn isValidHairColor(&self) -> bool {
+        return match HAIR_COLOR_RE.captures(&self.hairColor) {
+            Some(_cap) => true,
+            None => false
+         }
+    }
+
+    fn isValidId(&self) -> bool {
+        return match PASSPORT_ID_RE.captures(&self.passportID) {
+            Some(_cap) => true,
+            None => false
+         }
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -116,6 +222,126 @@ mod tests {
         hcl:#cfa07d eyr:2025 pid:166559648
         iyr:2011 ecl:brn hgt:59in";
         let result = INPUT.parse::<Day>().unwrap().partOne();
-        assert_eq!(result, 4);
+        assert_eq!(result, 2);
     }
-}
+
+    #[test]
+    fn fourPartTwoInvalidExampleTest() {
+        const INPUT: &str = "eyr:1972 cid:100
+        hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
+        
+        iyr:2019
+        hcl:#602927 eyr:1967 hgt:170cm
+        ecl:grn pid:012533040 byr:1946
+        
+        hcl:dab227 iyr:2012
+        ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
+        
+        hgt:59cm ecl:zzz
+        eyr:2038 hcl:74454a iyr:2023
+        pid:3556412378 byr:2007";
+        let result = INPUT.parse::<Day>().unwrap().partTwo();
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn fourPartTwoValidExampleTest() {
+        const INPUT: &str = "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
+        hcl:#623a2f
+        
+        eyr:2029 ecl:blu cid:129 byr:1989
+        iyr:2014 pid:7912057436 hcl:#a97842 hgt:165cm
+        
+        hcl:#888785
+        hgt:164cm byr:2001 iyr:2015 cid:88
+        pid:545766238 ecl:hzl
+        eyr:2022
+        
+        iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719";
+        let result = INPUT.parse::<Day>().unwrap().partTwo();
+        assert_eq!(result, 3);
+    }
+
+    fn testPassport() -> Passport {
+        return Passport {
+            birthYear: "1920".to_string(),
+            issueYear: "2012".to_string(),
+            expirationYear: "2030".to_string(),
+            height: "hgt:158cm".to_string(),
+            hairColor: "#623a2f".to_string(),
+            eyeColor: "grn".to_string(),
+            passportID: "087499704".to_string(),
+            countryID: None
+        };
+    }
+
+    #[test]
+    fn fieldValidationExampleTest() {
+        let mut passport = testPassport();
+        passport.birthYear = "1919".to_string();
+        assert_eq!(passport.validate().is_err(), true);
+        passport.birthYear = "2003".to_string();
+        assert_eq!(passport.validate().is_err(), true);
+        
+        // hgt valid:   60in
+        testHeight("60in", true);
+        // hgt valid:   190cm
+        testHeight("190cm", true);
+        // hgt invalid: 190in
+        testHeight("190in", false);
+        // hgt invalid: 190
+        testHeight("190", false);
+        // hcl valid:   #123abc
+        testHairColor("#123abc", true);
+        // hcl invalid: #123abz
+        testHairColor("#123abz", false);
+        // hcl invalid: 123abc
+        testHairColor("123abc", false);
+        // ecl valid:   brn
+        testEyeColor("brn", true);
+        // ecl invalid: wat
+        testEyeColor("wat", false);
+
+        // pid valid:   000000001
+        // pid invalid: 0123456789
+
+    }
+
+    fn testHeight(input: &str, expected: bool) {
+        let mut passport = testPassport();
+        passport.height = input.to_string();
+        if expected {
+            assert_eq!(passport.validate().is_err(), false);
+            assert_eq!(passport.validate().ok(), Some(true));
+        } else {
+            assert_eq!(passport.validate().is_ok(), false);
+            assert_eq!(passport.validate().is_err(), true);
+        }
+    }
+
+    fn testHairColor(input: &str, expected: bool) {
+        let mut passport = testPassport();
+        passport.hairColor = input.to_string();
+        let actual = passport.validate();
+        if expected {
+            assert_eq!(actual.is_err(), false);
+            assert_eq!(actual.ok(), Some(true));
+        } else {
+            assert_eq!(actual.is_ok(), false);
+            assert_eq!(actual.is_err(), true);
+        }
+    }
+
+    fn testEyeColor(input: &str, expected: bool) {
+        let mut passport = testPassport();
+        passport.eyeColor = input.to_string();
+        let actual = passport.validate();
+        if expected {
+            assert_eq!(actual.is_err(), false);
+            assert_eq!(actual.ok(), Some(true));
+        } else {
+            assert_eq!(actual.is_ok(), false);
+            assert_eq!(actual.is_err(), true);
+        }
+    }
+ }
