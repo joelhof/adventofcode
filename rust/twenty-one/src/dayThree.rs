b608bc751b3757322dc8b9295efe80a331e9d138
Joel <hofgren.joel@gmail.com>
2021-12-04 01:21:01 +0100

    2021, Day 3, part 2: Solved example.
    Lots ugly copy and paste...too tired to refactor to use functions as arguments.

--- rust/twenty-one/src/dayThree.rs
+++ rust/twenty-one/src/dayThree.rs
@@ -2,7 +2,7 @@
 
 pub fn partOne(input: &str) -> u32 {
     let bitSize = input.split("\n").count();
-    println!("Bit size {}", input.split("\n").count());
+    println!("Bit size {}", bitSize);
     
     let columnCount = match input.split("\n").next() {
         Some(line) => line.chars().count(),
@@ -24,23 +24,100 @@ pub fn partOne(input: &str) -> u32 {
             }
             return cols;
         });
+
     let gamma = u32::from_str_radix(
         &columns.iter()
             .map(|nr_of_ones| if *nr_of_ones > (bitSize as u32 / 2) { '1' } else { '0' })
             .fold(String::from(""), |mut nr, bit| { nr.push(bit); nr}), 2).unwrap();
+   
+    let b = getBitMask(columnCount);
+    println!("{} {}", gamma, !gamma & b);
 
-    let mut zeros = vec!['0'; 32-columnCount];
-    let mut ones = vec!['1'; columnCount];
-    zeros.append(&mut ones);
-    let bitmask: String = zeros.iter().collect();
-    println!("{}", bitmask);
-    println!("{} {}", gamma, !gamma & u32::from_str_radix(&bitmask, 2).unwrap());
+    return gamma * (!gamma & b);
+}
 
-    return gamma * (!gamma & u32::from_str_radix(&bitmask, 2).unwrap());
+fn bitCount(lines: &[&str]) -> Vec<u32> {
+    let len = lines[0].len();
+    //println!("nr of lines {:?} {}", lines.len(), (lines.len() as u32 / 2));
+    let x = lines.iter()
+    .map(|line| line.chars())
+    .fold(vec![0; len], |mut cols, chars| {
+        for (i, c) in chars.enumerate() {
+            cols[i] = match c {
+                '1' => cols[i] + 1,
+                _ => cols[i]
+            }
+        }
+
+        return cols;
+    });
+    println!("ones count {:?}", x);
+    return x;
 }
 
 pub fn partTwo(input: &str) -> u32 {
-    return 0;
+    
+    let numbers: Vec<&str> = input.split("\n")
+        .map(|line| line.trim())
+        .filter(|line| !line.is_empty())
+        .collect();
+
+    let oxygen_rating = recurCounterOxygen(&numbers, 0);
+    //println!("-------");
+    let co2_rating = recurCounterCarbonDioxide(&numbers, 0);
+    //println!("{} {}", oxygen_rating, co2_rating);
+    return u32::from_str_radix(&oxygen_rating, 2).unwrap() * u32::from_str_radix(&co2_rating, 2).unwrap();
+}
+
+fn recurCounterOxygen(numbers: &[&str], pos: usize) -> String {
+    if numbers.len() == 1 {
+        return numbers[0].to_string();
+    }
+    //println!("{:?}", numbers);
+    
+    let ones: Vec<char> = bitCount(numbers)
+        .iter()
+        .map(|nr_of_ones| if *nr_of_ones >= (numbers.len() as u32 - nr_of_ones) { '1' } else { '0' })
+        .collect();
+    //println!("pos {} {:?}", pos, ones);
+
+    
+    let next: Vec<&str> = numbers.iter()
+        .filter(|nr| nr.chars().nth(pos).unwrap() == ones[pos])
+        .map(|nr| *nr)
+        .collect();
+    return recurCounterOxygen(&next, pos + 1);
+}
+
+fn recurCounterCarbonDioxide(numbers: &[&str], pos: usize) -> String {
+    if numbers.len() == 1 {
+        return numbers[0].to_string();
+    }
+    //println!("{:?}", numbers);
+    
+    let ones: Vec<char> = bitCount(numbers)
+        .iter()
+        .map(|nr_of_ones| {
+            //println!("{} - {} = {}",numbers.len(), nr_of_ones, (numbers.len() as u32 - nr_of_ones));
+            if *nr_of_ones >= (numbers.len() as u32 - nr_of_ones) { '0' } else { '1' }
+        })
+        .collect();
+    //println!("pos {} {:?}", pos, ones);
+
+    
+    let next: Vec<&str> = numbers.iter()
+        .filter(|nr| nr.chars().nth(pos).unwrap() == ones[pos])
+        .map(|nr| *nr)
+        .collect();
+    return recurCounterCarbonDioxide(&next, pos + 1);
+}
+
+fn getBitMask(bitsize: usize) -> u32 {
+    let mut zeros = vec!['0'; 32-bitsize];
+    let mut ones = vec!['1'; bitsize];
+    zeros.append(&mut ones);
+    let bitmask: String = zeros.iter().collect();
+    return u32::from_str_radix(&bitmask, 2).unwrap();
 }
 
 #[cfg(test)]
