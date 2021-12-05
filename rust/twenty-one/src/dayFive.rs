cd86117c114e7370c662de0da7292fe84ee5cada
Joel <hofgren.joel@gmail.com>
2021-12-06 00:57:39 +0100

    2021, Day 5, part 1: Support diagonal lines.

--- rust/twenty-one/src/dayFive.rs
+++ rust/twenty-one/src/dayFive.rs
@@ -16,8 +16,11 @@ impl PartialEq for Coordinate {
 }
 
 impl Coordinate {
-    fn pointsOnLine(&self, other: &Coordinate) -> Vec<Coordinate> {
+    fn pointsOnLine(&self, other: &Coordinate, includeDiagonal: bool) -> Vec<Coordinate> {
         let mut points: Vec<Coordinate> = Vec::new();
+        let diffX: i64 = (other.0 as i64) - (self.0 as i64); 
+        let diffY: i64 = (other.1 as i64) - (self.1 as i64);
+        println!("{} {}", diffX, diffY);
         if self.0 == other.0 {
             println!("vertical line {:?} -> {:?}", self, other);
             let start = if self.1 >= other.1 { other.1 } else { self.1 };
@@ -32,6 +35,16 @@ impl Coordinate {
             for x in start..end {
                 points.push(Coordinate(x, self.1));
             }
+        } else if includeDiagonal && diffX.abs() == diffY.abs() {
+            println!("diagonal line {:?} -> {:?}", self, other);
+            for step in 0..diffX.abs() + 1 {
+                let x = self.0 as i64 + step * if diffX >= 0 { 1 } else { -1 };
+                let y = self.1 as i64 + step * if diffY >= 0 { 1 } else { -1 };
+                println!("x = {}, y = {}", x, y);
+                points.push(Coordinate(x as u32, y as u32));
+                
+            }
+
         }
 
         return points;
@@ -69,7 +82,7 @@ impl FromStr for VentMap {
                 .collect::<Result<Vec<Coordinate>, &str>>()
         )
         .map(|points| match points {
-            Ok(p) => p[0].pointsOnLine(&p[1]),
+            Ok(p) => p[0].pointsOnLine(&p[1], false),
             Err(_) => vec![]
         })
         .flatten()
@@ -87,6 +100,30 @@ pub fn partOne(input: &str) -> u32 {
     let vents: VentMap = input.parse().unwrap();
     println!("{}", vents.vents.keys().count());
     return vents.vents.values().filter(|nr_of_vents| **nr_of_vents >= 2).count() as u32;
+}
+
+pub fn partTwo(input: &str) -> u32 {
+    let vents: HashMap<Coordinate, u32> = input.lines()
+            .map(|l| l.trim())
+            .filter(|l| !l.is_empty())
+            .map(|l| l.split("->")
+                .map(|sub_str| sub_str.trim())
+                .filter(|sub_str| !sub_str.is_empty())
+                .map(|p| p.parse())
+                .collect::<Result<Vec<Coordinate>, &str>>()
+        )
+        .map(|points| match points {
+            Ok(p) => p[0].pointsOnLine(&p[1], true),
+            Err(_) => vec![]
+        })
+        .flatten()
+        .fold(HashMap::new(), |mut points, point| {
+            let counter = points.entry(point).or_insert(0);
+            *counter += 1;
+            return points
+        });
+    println!("{}", vents.keys().count());
+    return vents.values().filter(|nr_of_vents| **nr_of_vents >= 2).count() as u32;
 }
 
 #[cfg(test)]
@@ -119,7 +156,7 @@ mod tests {
             vec![Coordinate(0,0), Coordinate(8,8)]
         ];
         points.iter().for_each(|p| {
-            let points = p[0].pointsOnLine(&p[1]);
+            let points = p[0].pointsOnLine(&p[1], false);
             assert_eq!(0, points.len());    
         });
     }
@@ -131,11 +168,46 @@ mod tests {
             vec![Coordinate(1,4), Coordinate(3,4)]
         ];
         points.iter().for_each(|p| {
-            let points = p[0].pointsOnLine(&p[1]);
+            let points = p[0].pointsOnLine(&p[1], false);
             assert_eq!(3, points.len()); 
             assert_eq!(Coordinate(1,4), points[0]);
             assert_eq!(Coordinate(2,4), points[1]);
             assert_eq!(Coordinate(3,4), points[2]);   
         });
+    }
+
+    #[test]
+    fn partTwoExample() {
+        let input = "0,9 -> 5,9
+        8,0 -> 0,8
+        9,4 -> 3,4
+        2,2 -> 2,1
+        7,0 -> 7,4
+        6,4 -> 2,0
+        0,9 -> 2,9
+        3,4 -> 1,4
+        0,0 -> 8,8
+        5,5 -> 8,2
+        
+        ";
+        let res = partTwo(input);
+        assert_eq!(12, res);
+    }
+
+    #[test]
+    fn diagonalPointsOnLineTest() {
+        let points = vec![Coordinate(1,1), Coordinate(3,3)];
+        let linePoints = points[0].pointsOnLine(&points[1], true);
+            assert_eq!(3, linePoints.len()); 
+            assert_eq!(Coordinate(1,1), linePoints[0]);
+            assert_eq!(Coordinate(2,2), linePoints[1]);
+            assert_eq!(Coordinate(3,3), linePoints[2]);   
+
+        let points = vec![Coordinate(9,7), Coordinate(7,9)];
+        let linePoints = points[0].pointsOnLine(&points[1], true);
+            assert_eq!(3, linePoints.len()); 
+            assert_eq!(Coordinate(9,7), linePoints[0]);
+            assert_eq!(Coordinate(8,8), linePoints[1]);
+            assert_eq!(Coordinate(7,9), linePoints[2]);
     }
 }
