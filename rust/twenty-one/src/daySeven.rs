81b76bb615b76a8416e19732a042efd75eca29b3
Joel <hofgren.joel@gmail.com>
2021-12-07 21:24:23 +0100

    2021, Day 7, part 2: Refactor parseing for re-use.

--- rust/twenty-one/src/daySeven.rs
+++ rust/twenty-one/src/daySeven.rs
@@ -1,14 +1,28 @@
+use std::str::FromStr;
+
+struct Crabs {
+    positions: Vec<i32>
+}
+
+impl FromStr for Crabs {
+    type Err = &'static str;
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let positions: Vec<i32> = input.split(",")
+            .map(|sub_str| sub_str.trim())
+            .map(|nr| match nr.parse() { Err(_e) => { println!("{}", nr); -1 }, Ok(x) => x } )
+            .collect();
+        return Ok(Crabs { positions });
+    }
+}
 
 pub fn partOne(input: &str) -> i32 {
-    let positions: Vec<i32> = input.split(",")
-        .map(|sub_str| sub_str.trim())
-        .map(|nr| match nr.parse() { Err(_e) => { println!("{}", nr); -1 }, Ok(x) => x } )
-        .collect();
-
-    let max = match positions.iter().max() { Some(x) => *x, None => 0 };
+   
+    let crabs: Crabs = input.parse().unwrap();
+    let max = match crabs.positions.iter().max() { Some(x) => *x, None => 0 };
     let mut min_nr_of_steps = i32::MAX;
     for i in 0..max {
-        let delta: i32 = positions.iter()
+        let delta: i32 = crabs.positions.iter()
             .map(|pos| pos - i)
             .map(|delta| delta.abs())
             .sum();
@@ -21,6 +35,10 @@ pub fn partOne(input: &str) -> i32 {
     return min_nr_of_steps;
 }
 
+pub fn partTwo(input: &str) -> i32 {
+    return 0;
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -30,5 +48,12 @@ mod tests {
         let input = "16,1,2,0,4,2,7,1,2,14";
         let res = partOne(input);
         assert_eq!(37, res);
+    }
+    
+    #[test]
+    fn partTwoExample() {
+        let input = "16,1,2,0,4,2,7,1,2,14";
+        let res = partTwo(input);
+        assert_eq!(168, res);
     }
 }
