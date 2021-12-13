0565ab14c5b4695f2f244c8662e2c431f861c860
Joel <hofgren.joel@gmail.com>
2021-12-14 00:35:13 +0100

    2021, Day 12, part 2: Solved!

--- rust/twenty-one/src/dayTwelve.rs
+++ rust/twenty-one/src/dayTwelve.rs
@@ -33,7 +33,7 @@ impl Caves {
     }
 
     fn connect(&mut self, v: &str, w: &str) {
-        println!("connect {} to {}", v, w);
+        //println!("connect {} to {}", v, w);
         let neighbours = self.graph.entry(v.to_string()).or_insert(Vec::new());
         neighbours.push(w.to_string());
         if !self.is_connected(w, v) {
@@ -46,7 +46,7 @@ impl Caves {
     }
 
     fn depth_first_search<'a>(&mut self, start: &'a str, path: &'a mut Vec<&'a str>) {
-        println!("At Node: {}, path {:?}", start, path);
+        //println!("At Node: {}, path {:?}", start, path);
         path.push(start);
         if start == "end" {
             self.paths.push(path.iter().map(|s| s.to_string()).collect());
@@ -62,10 +62,48 @@ impl Caves {
             //.collect();
         //return paths;
     }
+
+    fn small_cave_constraint(cave: &str, path: &Vec<&str>) -> bool {
+        if !path.contains(&cave) {
+            return true;
+        } else if "start" == cave {
+            return false;
+        } else if "end" == cave {
+            return false;
+        }
+        else {
+            let mut freq = HashMap::new();
+            for small_cave in path.iter().filter(|c| !Caves::is_big_cave(c)) {
+                let f = freq.entry(small_cave).or_insert(0);
+                *f += 1;
+            };
+            //println!("{:?}", freq);
+            let existingDuplicates = freq.values().filter(|count| *count > &1).count();
+            return existingDuplicates == 0;
+        }
+    }
+
+    fn relaxed_small_cave<'a>(&mut self, start: &'a str, path: &'a mut Vec<&'a str>) {
+        //println!("relaxed_small_cave At Node: {}, path {:?}", start, path);
+        path.push(start);
+        if start == "end" {
+            self.paths.push(path.iter().map(|s| s.to_string()).collect());
+            return;
+        };
+
+        let neighbours = self.get_neighbours(start);
+        neighbours.iter()
+            .filter(|v| Caves::is_big_cave(v) || Caves::small_cave_constraint(v, path) )
+            .for_each(|v| self.relaxed_small_cave(v, &mut path.to_vec()));
+            //.flatten()
+            //.for_each
+            //.collect();
+        //return paths;
+    }
 }
 
 pub fn partOne(input: &str) -> u32 {
-    println!("PArt one input: {}", input);
+    //println!("PArt one input: {}", input);
     let mut caves: Caves = Caves { graph: HashMap::new(), paths: vec![] };
     input.lines()
         .map(|line| line.trim())
@@ -73,9 +111,24 @@ pub fn partOne(input: &str) -> u32 {
         .map(|line| line.split("-")
             .collect::<Vec<&str>>())
         .for_each(|nodes| caves.connect(nodes[0], nodes[1]));
-    println!("{:?}", caves.graph);
+    //println!("{:?}", caves.graph);
     caves.depth_first_search("start", &mut Vec::new());
-    caves.paths.iter().for_each(|path| println!("{:?}", path));
+    //caves.paths.iter().for_each(|path| println!("{:?}", path));
+    return caves.paths.len() as u32;
+}
+
+pub fn partTwo(input: &str) -> u32 {
+    //println!("PArt one input: {}", input);
+    let mut caves: Caves = Caves { graph: HashMap::new(), paths: vec![] };
+    input.lines()
+        .map(|line| line.trim())
+        .filter(|line| !line.is_empty())
+        .map(|line| line.split("-")
+            .collect::<Vec<&str>>())
+        .for_each(|nodes| caves.connect(nodes[0], nodes[1]));
+    //println!("{:?}", caves.graph);
+    caves.relaxed_small_cave("start", &mut Vec::new());
+    //caves.paths.iter().for_each(|path| println!("{:?}", path));
     return caves.paths.len() as u32;
 }
 
@@ -135,5 +188,59 @@ mod tests {
         start-RW";
         let res = partOne(input);
         assert_eq!(226, res);
+    }
+
+    #[test]
+    fn partTwoSmallExample() {
+        let input = "start-A
+        start-b
+        A-c
+        A-b
+        b-d
+        A-end
+        b-end
+        ";
+        let res = partTwo(input);
+        assert_eq!(36, res);
+    }
+    
+    #[test]
+    fn partTwoMediumExample() {
+        let input = "dc-end
+        HN-start
+        start-kj
+        dc-start
+        dc-HN
+        LN-dc
+        HN-end
+        kj-sa
+        kj-HN
+        kj-dc";
+        let res = partTwo(input);
+        assert_eq!(103, res);
+    }
+    
+    #[test]
+    fn partTwoBigExample() {
+        let input = "fs-end
+        he-DX
+        fs-he
+        start-DX
+        pj-DX
+        end-zg
+        zg-sl
+        zg-pj
+        pj-he
+        RW-he
+        fs-DX
+        pj-RW
+        zg-RW
+        start-pj
+        he-WI
+        zg-he
+        pj-fs
+        start-RW";
+        let res = partTwo(input);
+        assert_eq!(3509, res);
     }
 }
