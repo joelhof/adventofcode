ab08aa71347bca7c56dcbd01f84da6252f7d7b9c
Joel <hofgren.joel@gmail.com>
2021-12-17 00:45:57 +0100

    2021, Day 16, part 1: Parse a single operator.

--- rust/twenty-one/src/daySixteen.rs
+++ rust/twenty-one/src/daySixteen.rs
@@ -25,22 +25,43 @@ fn from_str<'a>(input: &str) -> Result<Packet, PacketErr> {
             _ => return Err("Unable to parse Packet, type ID must be 3 bits")
         }
     }
-    
-    if typeId == "100" {
+    let version = u8::from_str_radix(&version, 2).unwrap();
+    let typeId = u8::from_str_radix(&typeId, 2).unwrap();
+    if typeId == 4 {
         let value: String = bits.collect::<Vec<char>>().chunks(5)
             .map_while(|bit_group| if bit_group.len() == 5 {
                 Some(bit_group.iter().skip(1).collect::<String>())
                 } else { None }
             ).collect();
-        println!("{}", value);    
+        println!("{}", value);
         return Ok(Packet::LITERAL(Literal {
-            version: u8::from_str_radix(&version, 2).unwrap(), 
-            typeId: u8::from_str_radix(&typeId, 2).unwrap(), 
+            version: version,
+            typeId: typeId, 
             value: u32::from_str_radix(&value, 2).unwrap() 
         }));
     }
-
-    return Err("Unable to parse Packet");
+    
+    let mode = match bits.next() {
+        Some('0') => OperatorMode::FIFTEEN,
+        Some('1') => OperatorMode::ELEVEN,
+        _ => return Err("Unable to parse Packet, OperatorMode must be a bit")
+    };
+    let mut length_bits = Vec::new();
+    for _i in 0..mode.getLength() {
+        match bits.next() {
+            Some(c) if c == '0' || c == '1' => length_bits.push(c),
+            _ => return Err("Unable to parse Packet, nr of length bits is too small")
+        }
+    }
+    let length = usize::from_str_radix(&length_bits.iter().collect::<String>(), 2).unwrap();
+    let subPackets = bits.take(length).collect();
+    return Ok(Packet::OP(Operator {
+        mode: mode,
+        version: version,
+        typeId: typeId,
+        length: length,
+        packets: subPackets
+     }));
 }
 
 #[derive(Debug)]
@@ -66,10 +87,20 @@ enum OperatorMode {
     FIFTEEN
 }
 
+impl OperatorMode {
+    fn getLength(&self) -> usize {
+        return match self {
+            OperatorMode::ELEVEN => 11,
+            OperatorMode::FIFTEEN => 15
+        }
+    }
+}
+
 struct Operator {
     version: u8,
     typeId: u8,
-    packets: Vec<Packet>,
+    length: usize,
+    packets: String,
     mode: OperatorMode
 }
 
@@ -129,6 +160,20 @@ mod tests {
         assert_eq!(6, literal.version);
         assert_eq!(4, literal.typeId);
         assert_eq!(2021, literal.value);
+
+    }
+
+    #[test]
+    fn partOneOperatorExampleTest() {
+        let INPUT = hex_to_binary("38006F45291200");
+        let result = match from_str(&INPUT).unwrap() {
+            Packet::LITERAL(_) => None,
+            Packet::OP(op) => Some(op)
+        };
+        let operator = result.unwrap();
+        assert_eq!(1, operator.version);
+        assert_eq!(6, operator.typeId);
+        assert_eq!(27, operator.length);
 
     }
 }
