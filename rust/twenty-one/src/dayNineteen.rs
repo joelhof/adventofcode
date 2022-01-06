00ba5e8afeff27edd2ff425508ed7825f14c88a9
Joel <hofgren.joel@gmail.com>
2022-01-06 18:30:27 +0100

    2021, Day 19, part 1: There should 24 orientations.

--- rust/twenty-one/src/dayNineteen.rs
+++ rust/twenty-one/src/dayNineteen.rs
@@ -3,6 +3,7 @@ use std::str::FromStr;
 use std::collections::HashMap;
 use itertools::Itertools;
 use std::collections::HashSet;
+use std::iter::FromIterator;
 
 #[derive(Debug)]
 struct BeaconMap {
@@ -16,7 +17,7 @@ struct Scanner {
     beaconVectors: HashSet<Coordinate>
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, PartialOrd)]
+#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Clone)]
 struct Coordinate(i32,i32,i32);
 type CoordinateErr = &'static str;
 
@@ -53,7 +54,7 @@ impl FromStr for Coordinate {
             if coords.len() == 2 {
                 return Ok(Coordinate(coords[0], coords[1], 0));
             } else if coords.len() == 3 {
-                return Ok(Coordinate(coords[0], coords[1], coords[3]));
+                return Ok(Coordinate(coords[0], coords[1], coords[2]));
             }
         }
         return Err("Unable to parse into Coordinate");
@@ -68,7 +69,6 @@ impl FromStr for BeaconMap {
             .filter(|block| !block.is_empty())
             .enumerate()
             .map(|(i, block)| {
-                println!("{}: {}", i, block);
                 let scanners: Vec<Coordinate> = block.lines()
                     .map(|l| l.trim())
                     .filter(|l| !l.is_empty())
@@ -81,18 +81,17 @@ impl FromStr for BeaconMap {
     }
 }
 
-#[derive(Debug)]
-struct Rotation(Vec<Vec<i32>>);
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct Orientation(Vec<Vec<i32>>);
 
-impl Rotation {
-    fn axis_rotations() -> Vec<Rotation> {
-        let mut rotations: Vec<Rotation> = Vec::new();
-
-        for a in 1..=3 {
+impl Orientation {
+    fn axis_rotations() -> Vec<Orientation> {
+        let mut orientations: HashSet<Orientation> = HashSet::new();
+        for a in 0..=3 {
             let alpha = a as f32 * (std::f32::consts::PI / 2f32); 
-            for b in 1..=3 {
+            for b in 0..=3 {
                 let beta = b as f32 * (std::f32::consts::PI / 2f32); 
-                for g in 1..=3 {
+                for g in 0..=3 {
                     let gamma = g as f32 * (std::f32::consts::PI / 2f32);
                     let rot = vec![
                         vec![
@@ -111,12 +110,12 @@ impl Rotation {
                             (alpha.cos() * beta.cos()) as i32
                         ]
                     ];
-                    rotations.push(Rotation(rot));
+                    orientations.insert(Orientation(rot));
                 }
             }
         }
-        println!("nr of rotations {:?}", rotations.len());
-        return rotations;
+        println!("nr of rotations {:?}", orientations.len());
+        return Vec::from_iter(orientations.into_iter());
     }
 
     fn rotate(&self, columnVector: &Coordinate) -> Coordinate {
@@ -132,21 +131,17 @@ impl Rotation {
 
 pub fn partOne(input: &str) -> u32 {
     let map: BeaconMap = input.parse().unwrap();
-    println!("{:?}", map);
-    let rotations = Rotation::axis_rotations();
+    let rotations = Orientation::axis_rotations();
     let mut beaconToBeaconMap: HashMap<usize, HashSet<Coordinate>> = HashMap::new();
     for (scannerId, beacons) in map.scanners.iter() {
         let beaconToBeacon: HashSet<Coordinate> = beacons.iter()
             .tuple_combinations()
             .map(|(p1, p2)| {
-                println!("from {:?} to {:?}", p1, p2);
+                //println!("from {:?} to {:?}", p1, p2);
                 if p1 >= p2 { p1.createVector(p2) } else { p2.createVector(p1) }
             })
-            // .map(|vector| rotations.iter()
-            //     .map(move |r| r.rotate(&vector)))
-            // .flatten()
             .collect();
-        println!("scanner {} beacon to beacon vec {:?}, size {}", scannerId, beaconToBeacon, beaconToBeacon.len());
+        println!("scanner {} beacon to beacon, size {}", scannerId, beaconToBeacon.len());
         beaconToBeaconMap.insert(*scannerId, beaconToBeacon);
     }
     
@@ -164,19 +159,26 @@ pub fn partOne(input: &str) -> u32 {
                 add scanner j's mapped beacon coordinates to scanner i's beacons.
             else 
                 no overlap, continue to scanner j+1
+
+
+    
     */
     
     let mut uniqueBeacons: HashSet<Coordinate> = HashSet::new();
-    let scanner0 = beaconToBeaconMap.get(&1).unwrap();
-    let scanner1 = beaconToBeaconMap.get(&0).unwrap();
+    let scanner0 = beaconToBeaconMap.get(&0).unwrap();
+    let scanner1 = beaconToBeaconMap.get(&1).unwrap();
+    let mut sorted0 = Vec::from_iter(scanner0.iter());
+    sorted0.sort();
+    let mut sorted1 = Vec::from_iter(scanner1.iter());
+    sorted1.sort();
+    sorted0.iter().zip(sorted1.iter()).for_each(|(a,b)| println!("{:?}      {:?}", a,b));
     let intersection = scanner0.intersection(scanner1);
-    // println!("intersection: {:?}", intersection);
-    // for rotation in rotations.iter() {
-    //     let rotated: HashSet<Coordinate> = scanner0.iter().map(|v| rotation.rotate(v)).collect();
-    //     println!("{:?}", rotated);
-    //     let intersection = rotated.intersection(scanner1);
-    //     println!("{:?}", intersection);
-    // }
+    println!("intersection: {:?}", intersection);
+    for rotation in rotations.iter() {
+        let rotated: HashSet<Coordinate> = scanner0.iter().map(|v| rotation.rotate(v)).collect();
+        let intersection = rotated.intersection(scanner1);
+        println!("intersection {:?}", intersection);
+    }
 
     return intersection.count() as u32;
 }
@@ -202,7 +204,7 @@ mod tests {
     }
 
     #[test]
-    fn partOneTest() {
+    fn partOneMiniTest() {
         let input = "--- scanner 0 ---
         0,2
         4,1
@@ -212,6 +214,230 @@ mod tests {
         -1,-1
         -5,0
         -2,1";
+        let res = partOne(&input);
+        println!("{:?}", res);
+        assert_eq!(3, res);
+    }
+
+
+    #[test]
+    fn rotationTest() {
+        let input = "--- scanner 0 ---
+            -1,-1,1
+            -2,-2,2
+            -3,-3,3
+            -2,-3,1
+            5,6,-4
+            8,0,7";
+        let map: BeaconMap = input.parse().unwrap();
+        println!("{:?}", map);
+        
+        let expectedInput = "--- scanner 1 ---
+        1,-1,1
+        2,-2,2
+        3,-3,3
+        2,-1,3
+        -5,4,-6
+        -8,-7,0
+
+        --- scanner 2 ---
+        -1,-1,-1
+        -2,-2,-2
+        -3,-3,-3
+        -1,-3,-2
+        4,6,5
+        -7,0,8
+
+        --- scanner 3 ---
+        1,1,-1
+        2,2,-2
+        3,3,-3
+        1,3,-2
+        -4,-6,5
+        7,0,8
+
+        --- scanner 4 ---
+        1,1,1
+        2,2,2
+        3,3,3
+        3,1,2
+        -6,-4,-5
+        0,7,-8";
+        let expected: BeaconMap = expectedInput.parse().unwrap();
+        // put scanner 1-4 in a vector of HashSet<Coordinate>
+        let expectedRotations: Vec<HashSet<Coordinate>> = expected.scanners.values()
+            .map(|beacons| HashSet::from_iter(beacons.iter().cloned()))
+            .collect();
+        // for each rotation, put rotated points in a Vec<Hashset<Coordinates>>
+        let orientations = Orientation::axis_rotations();
+        assert_eq!(24, orientations.len());
+        orientations.iter().for_each(
+            |rot| {
+                println!("----");
+                map.scanners.get(&0).unwrap().iter().for_each(|v| println!("{:?}", rot.rotate(v)));
+            }
+        );
+        // for each expected Hashset compare against all the rotated ones, if all expected match rotations are ok
+
+
+        let v1 = Coordinate(-14, -87, 108); // => Coordinate(-14, 87, 108)
+        let v2 = Coordinate(-16, -16, 1299); // => Coordinate(-16, 16, 1299)
+        println!("{:?}", v1);
+        Orientation::axis_rotations().iter().for_each(
+            |rot| {
+                println!("----");
+                println!("{:?} {:?}", rot, rot.rotate(&v1));
+                if rot.rotate(&v1) == Coordinate(-14, 87, 108) {
+                    println!("{:?} => {:?} = {:?}", rot, v1, Coordinate(-14, 87, 108));
+                };
+                //rot.rotate(&v2);
+            }
+        );
+        // Coordinate(-33, 26, -1227) => Coordinate(-33, -26, -1227)
+        // Coordinate(-36, 6, 33) => Coordinate(-36, -6, 33)
+        // Coordinate(-43, 8, 46) => Coordinate(-43, -8, 46)
+        //println!("{:?}", res);
+        assert_eq!(3, map.scanners.len());
+    }
+
+    #[test]
+    fn partOneOverlapTest() {
+        let input = "--- scanner 0 ---
+        404,-588,-901
+        528,-643,409
+        -838,591,734
+        390,-675,-793
+        -537,-823,-458
+        -485,-357,347
+        -345,-311,381
+        -661,-816,-575
+        -876,649,763
+        -618,-824,-621
+        553,345,-567
+        474,580,667
+        -447,-329,318
+        -584,868,-557
+        544,-627,-890
+        564,392,-477
+        455,729,728
+        -892,524,684
+        -689,845,-530
+        423,-701,434
+        7,-33,-71
+        630,319,-379
+        443,580,662
+        -789,900,-551
+        459,-707,401
+        
+        --- scanner 1 ---
+        686,422,578
+        605,423,415
+        515,917,-361
+        -336,658,858
+        95,138,22
+        -476,619,847
+        -340,-569,-846
+        567,-361,727
+        -460,603,-452
+        669,-402,600
+        729,430,532
+        -500,-761,534
+        -322,571,750
+        -466,-666,-811
+        -429,-592,574
+        -355,545,-477
+        703,-491,-529
+        -328,-685,520
+        413,935,-424
+        -391,539,-444
+        586,-435,557
+        -364,-763,-893
+        807,-499,-711
+        755,-354,-619
+        553,889,-390
+        
+        --- scanner 2 ---
+        649,640,665
+        682,-795,504
+        -784,533,-524
+        -644,584,-595
+        -588,-843,648
+        -30,6,44
+        -674,560,763
+        500,723,-460
+        609,671,-379
+        -555,-800,653
+        -675,-892,-343
+        697,-426,-610
+        578,704,681
+        493,664,-388
+        -671,-858,530
+        -667,343,800
+        571,-461,-707
+        -138,-166,112
+        -889,563,-600
+        646,-828,498
+        640,759,510
+        -630,509,768
+        -681,-892,-333
+        673,-379,-804
+        -742,-814,-386
+        577,-820,562
+        
+        --- scanner 3 ---
+        -589,542,597
+        605,-692,669
+        -500,565,-823
+        -660,373,557
+        -458,-679,-417
+        -488,449,543
+        -626,468,-788
+        338,-750,-386
+        528,-832,-391
+        562,-778,733
+        -938,-730,414
+        543,643,-506
+        -524,371,-870
+        407,773,750
+        -104,29,83
+        378,-903,-323
+        -778,-728,485
+        426,699,580
+        -438,-605,-362
+        -469,-447,-387
+        509,732,623
+        647,635,-688
+        -868,-804,481
+        614,-800,639
+        595,780,-596
+        
+        --- scanner 4 ---
+        727,592,562
+        -293,-554,779
+        441,611,-461
+        -714,465,-776
+        -743,427,-804
+        -660,-479,-426
+        832,-632,460
+        927,-485,-438
+        408,393,-506
+        466,436,-512
+        110,16,151
+        -258,-428,682
+        -393,719,612
+        -211,-452,876
+        808,-476,-593
+        -575,615,604
+        -485,667,467
+        -680,325,-822
+        -627,-443,-432
+        872,-547,-609
+        833,512,582
+        807,604,487
+        839,-516,451
+        891,-625,532
+        -652,-548,-490
+        30,-46,-14";
         let res = partOne(&input);
         println!("{:?}", res);
         assert_eq!(3, res);
